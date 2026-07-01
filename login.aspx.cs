420fe9cec07ddfb31581e1fde231d25875bbcde4
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-07-01 08:10:03 +0530

    Add files via upload

---
+++ login.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.UI;
+
+namespace UNIT1
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string username = txtUser.Text;
+
+            Response.Write("<script>alert('Login Successfully... Welcome " + username + "');</script>");
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
