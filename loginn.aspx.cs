1f0d8b7d62d6878a749f6f4207a86d114d3ef656
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-07-29 21:03:18 +0530

    Add files via upload

---
+++ loginn.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class loginn1 : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nipun\OneDrive\Documents\registraion.mdf;Integrated Security=True;Connect Timeout=30");
+
+            con.Open();
+
+            cmd = new SqlCommand("INSERT INTO Student1 (Username,Password) VALUES (@Username,@Password)", con);
+
+            cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            Response.Redirect("Display_data.aspx");
+        }
+    }
+}
