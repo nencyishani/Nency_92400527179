86dc116dea6628d68907985f21eda3943ff2ff6b
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-07-29 21:03:55 +0530

    Add files via upload

---
+++ Display_data.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Display_data : System.Web.UI.Page
+    {
+        SqlConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nipun\OneDrive\Documents\registraion.mdf;Integrated Security=True;Connect Timeout=30");
+
+                con.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Student1", con);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+
+                con.Close();
+            }
+        }
+    }
+}
